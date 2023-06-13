4f201160ba7ded0aaead199b315317e454854863
VassBassApp <vassbassapp@gmail.com>
2023-06-13 22:35:31 +0300

    I'm run it!

--- app/src/main/java/com/vassbassapp/cmc_mobile/MainActivity.kt
+++ app/src/main/java/com/vassbassapp/cmc_mobile/MainActivity.kt
@@ -1,49 +1,58 @@
 package com.vassbassapp.cmc_mobile
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
+import android.view.View.OnClickListener
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
-import android.widget.Button
-import android.view.View.OnClickListener
+import androidx.appcompat.app.AppCompatActivity
+import com.vassbassapp.cmc_mobile.model.Control
+import com.vassbassapp.cmc_mobile.repository.control.ControlRepository
+import com.vassbassapp.cmc_mobile.repository.control.LocalSqliteControlRepository
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
-    private val repository : ControlRepository = LocalSqliteControlRepository(this, null)
+    private lateinit var repository : ControlRepository
     private val list = mutableListOf<String>()
-    private val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+    private lateinit var adapter : ArrayAdapter<String>
 
-    private val controlName : EditText = findViewById(R.id.controlName)
-    private val controlList : ListView = findViewById(R.id.controlList)
-    private val btnAdd : Button = findViewById(R.id.btnAdd)
+    private lateinit var controlName : EditText
+    private lateinit var controlList : ListView
+    private lateinit var btnAdd : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        repository = LocalSqliteControlRepository(this, null)
 
-        controlList.setAdapter(adapter)
+        controlName = findViewById(R.id.controlName)
+        controlList = findViewById(R.id.controlList)
+        btnAdd = findViewById(R.id.btnAdd)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        controlList.adapter = adapter
 
         btnAdd.setOnClickListener(addBtnClickListener)
         refreshControlList()
     }
 
-    fun refreshControlList() {
+    private fun refreshControlList() {
         list.clear()
         list.addAll(repository.getAll().map { it.toString() } )
         adapter.notifyDataSetChanged()
     }
 
-    val addBtnClickListener = object : OnClickListener() {
-        override fun onClick(v: View!) {
-            val name = controlName.getText().toString()
-            if (name.isNotBlank()) {
-                val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
-                Control control = Control(null, null, name, date)
-                repository.add(control)
-                refreshControlList()
-                controlName.setText("")
-            }
+    private val addBtnClickListener = OnClickListener {
+        val name = controlName.text.toString()
+        if (name.isNotBlank()) {
+            val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
+            val control = Control(null, null, name, date)
+            repository.add(control)
+            refreshControlList()
+            controlName.setText("")
         }
     }
 }
